348ad97182e52c64e4ccdd32cedfb421d18d3c8f
qiaw99 <872907968@qq.com>
2022-04-20 21:11:37 +0200

    [Commit] Add PCA and Gammaidx

---
+++ problem_set1/stubs/ps1_application.py
@@ -0,0 +1,42 @@
+""" sheet1_implementation.py
+
+PUT YOUR NAMES HERE:
+<FIRST_NAME><LAST_NAME>
+<FIRST_NAME><LAST_NAME>
+
+
+Write the functions
+- usps
+- outliers
+- lle
+Write your implementations in the given functions stubs!
+
+
+(c) Daniel Bartz, TU Berlin, 2013
+    Jacob Kauffmann, TU Berlin, 2021
+"""
+import numpy as np
+import ps1_implementation as imp
+
+def usps():
+    ''' performs the usps analysis for assignment 5'''
+
+
+def outliers_calc():
+    ''' outlier analysis for assignment 6'''
+    # np.savez_compressed('outliers.npz', var1=var1, var2=var2, ...)
+
+
+def outliers_disp():
+    ''' display the boxplots'''
+    # results = np.load('outliers.npz')
+
+
+def lle_visualize(dataset='flatroll'):
+    ''' visualization of LLE for assignment 7'''
+
+
+def lle_noise():
+    ''' LLE under noise for assignment 8'''    
+
+
